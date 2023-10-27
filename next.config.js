3db122db19e330bfdb966f1cae42860d156537bd
Pranali <malkarpg@gmail.com>
2023-10-27 17:40:10 +0530

    fix unsaved changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-        appDir: true,
-    },
     images: {
         domains: ['links.papareact.com', 'lh3.googleusercontent.com']
     }
